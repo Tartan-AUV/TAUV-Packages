6510fc310d1e3769eff1c31ccd3c64cda02bc693
Gleb Ryabtsev <gryabtse@andrew.cmu.edu>
2024-11-15 19:31:09 -0500

    done, no docs

--- containers/scripts/run-kingfisher.sh
+++ containers/scripts/run-kingfisher.sh
@@ -8,7 +8,8 @@ docker run \
   -v /dev:/dev \
   -v /tm.X11-unix/:/tmp/.X11-unix \
   -v /tmp/.docker.xauth:/tmp/.docker.xauth \
-  -v /home/tartanauv/tauv-mono/packages:/shared/tauv_ws/src/tauv-packages \
+  -v /home/tartanauv/tauv-mono/packages:/tauv-packages \
+  -v /home/tartanauv/shared:/shared \
   -e XAUTHORITY=/tmp/.docker.xauth \
   --net=host \
   -it \
