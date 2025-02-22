3f9d8bda96099f17319dd10e111c853d784c73c4
Gleb Ryabtsev <gryabtse@andrew.cmu.edu>
2025-02-22 14:19:57 -0500

    Move packages to packages-legacy. Add tauv_sim ROS2 package to packages

--- packages/tauv_sim/setup.py
+++ packages/tauv_sim/setup.py
@@ -1,11 +1,26 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import find_packages, setup
 
-setup_args = generate_distutils_setup(
-    packages=['cameras'],
-    package_dir={'': 'src'},
-    requires=['rospy'],
-    scripts=['scripts/disparity_to_depth']
+package_name = 'tauv_sim'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='gleb',
+    maintainer_email='gryabtse@andrew.cmu.edu',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'sim = tauv_sim.sim:main'
+        ],
+    },
 )
-
-setup(**setup_args)
